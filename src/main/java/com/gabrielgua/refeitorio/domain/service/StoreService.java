b1a04da38e63370933a6126f1eb7737e2868720a
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2025-03-17 15:06:46 -0300

    chore: add store listing endpoint

--- src/main/java/com/gabrielgua/refeitorio/domain/service/StoreService.java
+++ src/main/java/com/gabrielgua/refeitorio/domain/service/StoreService.java
@@ -7,11 +7,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class StoreService {
 
     private final StoreRepository storeRepository;
+
+    @Transactional(readOnly = true)
+    public List<Store>  findAll() {
+        return storeRepository.findAll();
+    }
 
     @Transactional(readOnly = true)
     public Store findById(Long id) {
